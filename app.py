88f23abeea92154a9256cf054a5c75e9d80571fd
monmacllc <quanzourei@gmail.com>
2025-05-25 02:28:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import json
 from datetime import datetime, timedelta
+from dateutil.parser import parse as dtparser
 from openai import OpenAI
 import os
 import gspread
